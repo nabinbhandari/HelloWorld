a2a95f3f629655f8abcd11d1776631086cc1c368
Nabin Bhandari <bnabin51@gmail.com>
2018-07-03 01:01:07 +0545

    Implement basic sliding button.

--- app/src/main/java/com/nabinbhandari/helloworld/MainActivity.java
+++ app/src/main/java/com/nabinbhandari/helloworld/MainActivity.java
@@ -2,6 +2,10 @@ package com.nabinbhandari.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SwipeButton swipeButton = findViewById(R.id.swipeBtn);
+        final TextView textView = findViewById(R.id.textView);
+
+        swipeButton.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public void onStateChange(boolean active) {
+                textView.setText(/*active*/swipeButton.isActive() ? "Active" : "Inactive");
+            }
+        });
     }
 
 }
